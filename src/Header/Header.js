cf605c31dda3fa5dbc0038383e9a50744c56391b
Betina <zh.betina@gmail.com>
2021-01-18 03:14:14 +0100

    Improve: styling for header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -16,12 +16,22 @@ const Header = () => {
                     <input className="Header-input" type="number"></input> pages.
                 </label>
             </div>
-            <div className="Header-col">
-                <PrimaryButton txt="Save my reading progress" />
-                <PrimaryButton txt="Don't save it and carry on" />
+            <div className="Header-col-btns">
+                <Link to="/profile"><PrimaryButton txt="Save my reading progress" /></Link>
+                <Link to="/profile"><PrimaryButton txt="Don't save it and carry on" /></Link>
             </div>
         </header>
     )
 }
+
+/* todo: 
+1) add event on the btns:
+after clicking on any of them, two options will appear: to login in/to create an account
+2) center the label with the input
+3) adapt input styling
+4) handle pages saving (Q for backend: can we/do we have an endpoint where no of pages are updated
+    instead of being replaced and added as a reader with a new id? Probably need a relation)
+    */
+
 
 export default Header;
