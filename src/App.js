83ac105decbd6864ef78f3864d00ec6bee1f23a7
Betina <zh.betina@gmail.com>
2021-01-19 01:01:05 +0100

    Add: footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter } from 'react-router-dom';
 
 import './App.scss';
 import Views from './Views/Views';
+import Footer from './Footer/Footer';
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className="App">
         <Views />
+        <Footer />
       </div>
     </BrowserRouter>
   );
